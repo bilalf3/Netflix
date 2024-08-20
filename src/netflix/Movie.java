bc2399a336a1c4797a9c48393b02b411e7e79e16
bilalf3 <72197187+bilalf3@users.noreply.github.com>
2024-08-20 14:21:14 +0300

    Movie.java

--- src/netflix/Movie.java
+++ src/netflix/Movie.java
@@ -1,23 +1,31 @@
 package netflix;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Movie extends Material {
     ArrayList<Person> actors;
     Person director;
 
-    public Movie(int id, String name, ArrayList<Integer> scores, int releaseYear, int price, Category category, String type, ArrayList<Person> actors, Person director) {
-        super(id, name, scores, releaseYear, price, category, type);
+    public Movie(int id, String name, ArrayList<Integer> scores, int release_year, int price, Category category, String type, ArrayList<Person> actors, Person director) {
+        super(id, name, scores, release_year, price, category, type);
         this.actors = actors;
         this.director = director;
     }
 
+    // Aktör ekleme işlemi
     public void addActor(Person actor) {
         this.actors.add(actor);
     }
 
     @Override
-    void showDetail() {
-        System.out.println("Film: " + name + ", Yonetmen: " + director.firstName + " " + director.lastName + ", Ortalama Puan: " + getAvgScore());
+    public void showDetail() {
+        System.out.println("Film:  " + name);
+        System.out.println("Yapimci: " + director.firstName + " " + director.lastName);
+        System.out.println("Aktor: ");
+        actors.stream()
+                .forEach(actor -> System.out.println(actor.firstName + " " + actor.lastName));
+        System.out.println("Ortalama Skor: " + getAvgScore());
+        System.out.println("Fiyat: ₺" + price);
     }
 }
