8ac1cbd4b4940824dea80024c8623253958b17bd
bilalf3 <72197187+bilalf3@users.noreply.github.com>
2024-08-20 14:22:05 +0300

    User.java

--- src/netflix/User.java
+++ src/netflix/User.java
@@ -2,7 +2,7 @@ package netflix;
 
 public class User extends Person {
     String userName;
-    String password;
+    String  password;
 
     public User(int id, String firstName, String lastName, String userName, String password) {
         super(id, firstName, lastName, 0); 
